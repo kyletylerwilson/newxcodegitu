9f82f1e3d32ca38bc60cd5972bc08d54146cb36c
Kyle Wilson <kyletylerwilson@gmail.com>
2023-03-31 19:19:44 -0400

    Committing

--- newxcodegit/ContentView.swift
+++ newxcodegit/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.red)
-            Text("Hello, world today!")
+            Text("Hello, world to")
         }
         .padding()
     }
