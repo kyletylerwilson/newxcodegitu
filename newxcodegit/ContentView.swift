d4f593e68b6f909364210ec2334fe3a9d33bd761
Kyle Wilson <kyletylerwilson@gmail.com>
2023-03-31 19:00:05 -0400

    Initial Commit

---
+++ newxcodegit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  newxcodegit
+//
+//  Created by Kyle Wilson on 2023-03-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.red)
+            Text("Hello, world today!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ newxcodegit/newxcodegitApp.swift
@@ -0,0 +1,17 @@
+//
+//  newxcodegitApp.swift
+//  newxcodegit
+//
+//  Created by Kyle Wilson on 2023-03-31.
+//
+
+import SwiftUI
+
+@main
+struct newxcodegitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
